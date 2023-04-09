a06e8060f5b8b900b738b6aef107e1e8afdfca53
jamol1027 <jamol40816>
2023-04-09 13:02:38 +0500

    5

--- begin05.py
+++ begin05.py
@@ -4,4 +4,10 @@
 
 # Assign 5 (int value) to 'c' variable;
 
-# Print the sum of three variables 'a', 'b', 'c';
+# Print the sum of three variables 'a', 'b', 'c'; 
+
+a = 7
+b = 3
+c = 5
+print(a+b+c)
+
